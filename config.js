5a240e3912ee0ad883dffcfeb54c4c066616b028
Tokpish <tokpish@gmail.com>
2026-06-26 14:24:31 +0300

    Connect Firebase project

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-window.SHOPSYNC_FIREBASE_CONFIG = null;
+window.SHOPSYNC_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAly53LX2Xea9lqzbEjbCKHW-C6vCMXFGg",
+  authDomain: "shared-shopping-eb266.firebaseapp.com",
+  databaseURL: "https://shared-shopping-eb266-default-rtdb.firebaseio.com",
+  projectId: "shared-shopping-eb266",
+  storageBucket: "shared-shopping-eb266.firebasestorage.app",
+  messagingSenderId: "10678337748",
+  appId: "1:10678337748:web:ab289185fab031f9c93b41"
+};
